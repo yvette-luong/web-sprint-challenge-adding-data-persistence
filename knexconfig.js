2daae71a827b9d6ca23074b54959abf2eec56ef3
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-06 11:06:24 -0800

    Create knexconfig.js

---
+++ knexconfig.js
@@ -0,0 +1,10 @@
+const knex = require("knex")
+
+const knexfile = require('../knexfile')
+
+//on heroku NODE_ENV will be 'production'
+const enviroment = process.env.NODE_ENV ||"development";
+
+const config = knexfile[enviroment];
+
+module.exports = knex(config); 
