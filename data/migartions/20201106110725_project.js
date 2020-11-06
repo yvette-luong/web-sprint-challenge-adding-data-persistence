1f0b754a27e5f00323a7aafef500dba9008526e6
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-06 11:42:42 -0800

    tables work

---
+++ data/migartions/20201106110725_project.js
@@ -0,0 +1,56 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("projects", (tbl) => {
+      tbl.increments(); //project id
+      tbl.string("project_name", 169).notNullable().unique(); //project name
+      tbl.boolean("mvp"); //project completed
+      tbl.string("project_description", 250).notNullable(); // prj description
+    })
+
+    .createTable("resources", (tbl) => {
+      tbl.increments(); //resource id
+      tbl.string("res_name", 169).notNullable().unique(); //resourse name
+      tbl.string("res_description", 250).notNullable(); // prj description
+
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .references("id")
+        .inTable("projects")
+        .onDelete("RESTRICT") // CASCADE, RESTRICT
+        .onUpdate("CASCADE");
+    })
+
+    .createTable("tasks", (tbl) => {
+      tbl.increments(); //task id
+      tbl.string("task_name", 169).notNullable().unique(); //task name
+      tbl.string("task_description", 250).notNullable(); // task description
+      tbl.boolean("task_mvp"); //task completed
+      tbl.string("note", 169).notNullable(); //project name
+
+      tbl
+        .integer("project_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("projects")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+
+      tbl
+        .integer("resource_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("resources")
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects");
+};
