0d4495ad203d522fd2f59d5126ba6487fb8f955c
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-11-11 09:41:01 -0800

    Update resourceRouter.js

--- api/resourceRouter.js
+++ api/resourceRouter.js
@@ -1,3 +1,18 @@
 const express = require("express");
 
 const Res = require('../router/project-model')
+const resRouter = express.Router()
+
+resRouter.get('/', (req,res) =>{
+
+})
+
+resRouter.get('/:id', (req,res) =>{
+
+})
+
+resRouter.post('/', (req,res) =>{
+
+})
+
+module.exports = resRouter
